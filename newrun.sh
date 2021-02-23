f3f8d34f2bb99e701259aba186d7a41d372466ed
Pengxiang Zhang <z-git@protonmail.com>
2021-02-23 11:51:57 -0600

    combine to a single python file

--- newrun.sh
+++ newrun.sh
@@ -1,15 +1,15 @@
-~/TrimGalore-0.6.6/trim_galore  --illumina -j 12 --paired sample_R1.fq.gz sample_R2.fq.gz
-
-#1 bowtie2
+#0 unpack
+trim_galore  --illumina -j 12 --paired sample_R1.fq.gz sample_R2.fq.gz
 seqtk seq -a sample_R1_val_1.fq.gz  > sample_R1.fa
 seqtk seq -a sample_R2_val_2.fq.gz  > sample_R2.fa
+
+#1 bowtie2
 bowtie2 -p 32 -x /mnt/array2/jinfang/dbcan_da/grant_test/CAZyDB.version2.seperated sample_R1_val_1.fq.gz -S bowtie.R1.sam
 bowtie2 -p 32 -x /mnt/array2/jinfang/dbcan_da/grant_test/CAZyDB.version2.seperated sample_R2_val_2.fq.gz -S bowtie.R2.sam
 # TODO: Change CAZyDB location.
 bioconvert sam2paf bowtie.R1.sam output/bowtie.R1.paf
 bioconvert sam2paf bowtie.R2.sam output/bowtie.R2.paf
 python3 paf_result_analysis.py output/bowtie.R1.paf bowtie.R2.paf output/result/bowtie.sequence_FPKM.json result/bowtie.cazyfamily_FPKM.json
-# cat bowtie.R1.paf  bowtie.R2.paf > bowtie.paf
 
 #2 diamond
 diamond blastx --strand both --evalue 1e-6 --query sample_R1.fa --db ../dbcan --threads 32 --out output/diamond.R1.paf --outfmt 103 
@@ -21,4 +21,5 @@ python3 paf_result_analysis.py output/diamond.new.R1.paf output/diamond.new.R2.p
 #3 minimap2
 minimap2 -t 32 -x sr gold_standard_genome.fa sample_R1_val_1.fq.gz sample_R2_val_2.fq.gz > output/minimap2.paf
 
+rm output/*
 rm sample_R1_val_1.fq.gz sample_R2_val_2.fq.gz sample_R1.fa sample_R2.fa bowtie.R1.sam bowtie.R2.sam
--- paf_result_analysis.py
+++ paf_result_analysis.py
@@ -1,7 +1,13 @@
 import json
 import time
-
+import csv
+import sys
 from pafpy import PafFile
+
+# Input(argv1): R1 paf File
+# Input(argv2): R2 paf File
+# Output(argv3): sequence_FPKM csv file
+# Output(argv4): cazyfamily_FPKM csv file
 
 
 def get_brief_records(path, cazyfamilies, sequences):
@@ -10,9 +16,7 @@ def get_brief_records(path, cazyfamilies, sequences):
         paf = PafFile(fileobj)
         for record in paf:
             sequence = record.tname.split('|')[0]
-            if sequence in sequences['name_list']:
-                print("the sequence exists")
-            else:
+            if sequence not in sequences['name_list']:
                 seq_info = {}
                 seq_info['seq_read_count'] = 0
                 seq_info['seq_length'] = record.tlen
@@ -22,28 +26,30 @@ def get_brief_records(path, cazyfamilies, sequences):
             multi_cazyfamilies = record.tname.split('|')[1:]
             if len(multi_cazyfamilies) > 1:
                 del multi_cazyfamilies[-1]
-            print('multi_cazyfamilies:' + str(multi_cazyfamilies))
+
             for cazyfamily in multi_cazyfamilies:
                 if cazyfamily in cazyfamilies['name_list']:
-                    print("the cazyfamily exists")
                     seq_array = [sequence, record.tlen]
                     if seq_array not in cazyfamilies['name_list'][cazyfamily]['cazyfamily_sequence_list']:
                         cazyfamilies['name_list'][cazyfamily]['cazyfamily_sequence_list'].append(seq_array)
+                        cazyfamilies['name_list'][cazyfamily]['seq_amount'] += 1
                 else:
                     cazy_info = {}
                     cazy_info['cazyfamily_read_count'] = 0
                     seq_array = [sequence, record.tlen]
                     cazy_info['cazyfamily_sequence_list'] = [seq_array]
+                    cazy_info['seq_amount'] = 1
                     cazyfamilies['name_list'][cazyfamily] = cazy_info
                     cazyfamilies['family_amount'] += 1
 
             read_id = record.qname.split('/')[0]
             if read_id in reads.keys():
-                print('the read_id exists in the list')
+                reads[read_id]['seq_id'].append(record.tname.split('|')[0])
+                reads[read_id]['cazy_family'] += multi_cazyfamilies
             else:
                 read = {}
                 read['cazy_family'] = multi_cazyfamilies
-                read['seq_id'] = record.tname.split('|')[0]
+                read['seq_id'] = [record.tname.split('|')[0]]
                 reads[read_id] = read
 
     output_name = path.split('.paf')[0] + '.json'
@@ -54,29 +60,14 @@ def get_brief_records(path, cazyfamilies, sequences):
     return reads
 
 def cazy_family_counter(cazyfamilies, reads_R1, reads_R2, seq_id_intersec, seq_id_list_R1_subtraction, seq_id_list_R2_subtraction):
-    # for read_id in reads_R1.keys():
-    #     if read_id in reads_R2.keys():
-    #         read_in_R1 = reads_R1[read_id]
-    #         for read1_cazyfamily in read_in_R1['cazy_family']:
-    #             cazyfamilies['name_list'][read1_cazyfamily]['cazyfamily_read_count'] += 0.5
-    #     else:
-    #         read_in_R1 = reads_R1[read_id]
-    #         for read1_cazyfamily in read_in_R1['cazy_family']:
-    #             cazyfamilies['name_list'][read1_cazyfamily]['cazyfamily_read_count'] += 1
-    #
-    # for read_id in reads_R2.keys():
-    #     if read_id in reads_R1.keys():
-    #         read_in_R2 = reads_R2[read_id]
-    #         for read2_cazyfamily in read_in_R2['cazy_family']:
-    #             cazyfamilies['name_list'][read2_cazyfamily]['cazyfamily_read_count'] += 0.5
-    #     else:
-    #         read_in_R2= reads_R2[read_id]
-    #         for read2_cazyfamily in read_in_R2['cazy_family']:
-    #             cazyfamilies['name_list'][read2_cazyfamily]['cazyfamily_read_count'] += 1
     for read_id in seq_id_intersec:
         read_in_R1 = reads_R1[read_id]
         for read1_cazyfamily in read_in_R1['cazy_family']:
             cazyfamilies['name_list'][read1_cazyfamily]['cazyfamily_read_count'] += 1
+        read_in_R2 = reads_R2[read_id]
+        for read2_cazyfamily in read_in_R2['cazy_family']:
+            if read2_cazyfamily not in read_in_R1['cazy_family']:
+                cazyfamilies['name_list'][read2_cazyfamily]['cazyfamily_read_count'] += 1
 
     for read_id in seq_id_list_R1_subtraction:
         read_in_R1 = reads_R1[read_id]
@@ -99,39 +90,29 @@ def cazy_family_counter(cazyfamilies, reads_R1, reads_R2, seq_id_intersec, seq_i
 
 
 def sequence_counter(sequences, reads_R1, reads_R2, seq_id_intersec, seq_id_list_R1_subtraction, seq_id_list_R2_subtraction):
-    # for read_id in reads_R1.keys():
-    #     if read_id in reads_R2.keys():
-    #         read_in_R1 = reads_R1[read_id]
-    #         read1_sequence = read_in_R1['seq_id']
-    #         sequences['name_list'][read1_sequence]['seq_read_count'] += 0.5
-    #     else:
-    #         read_in_R1 = reads_R1[read_id]
-    #         read1_sequence = read_in_R1['seq_id']
-    #         sequences['name_list'][read1_sequence]['seq_read_count'] += 1
-    #
-    # for read_id in reads_R2.keys():
-    #     if read_id in reads_R1.keys():
-    #         read_in_R2 = reads_R2[read_id]
-    #         read2_sequence = read_in_R2['seq_id']
-    #         sequences['name_list'][read2_sequence]['seq_read_count'] += 0.5
-    #     else:
-    #         read_in_R2 = reads_R2[read_id]
-    #         read2_sequence = read_in_R2['seq_id']
-    #         sequences['name_list'][read2_sequence]['seq_read_count'] += 1
     for read_id in seq_id_intersec:
         read_in_R1 = reads_R1[read_id]
-        read1_sequence = read_in_R1['seq_id']
-        sequences['name_list'][read1_sequence]['seq_read_count'] += 1
+        read1_sequences = read_in_R1['seq_id']
+        for read1_sequence in read1_sequences:
+            sequences['name_list'][read1_sequence]['seq_read_count'] += 1
+
+        read_in_R2 = reads_R2[read_id]
+        read2_sequences = read_in_R2['seq_id']
+        for read2_sequence in read2_sequences:
+            if read2_sequence not in read1_sequence:
+                sequences['name_list'][read2_sequence]['seq_read_count'] += 1
 
     for read_id in seq_id_list_R1_subtraction:
         read_in_R1 = reads_R1[read_id]
-        read1_sequence = read_in_R1['seq_id']
-        sequences['name_list'][read1_sequence]['seq_read_count'] += 1
+        read1_sequences = read_in_R1['seq_id']
+        for read1_sequence in read1_sequences:
+            sequences['name_list'][read1_sequence]['seq_read_count'] += 1
 
     for read_id in seq_id_list_R2_subtraction:
         read_in_R2 = reads_R2[read_id]
-        read2_sequence = read_in_R2['seq_id']
-        sequences['name_list'][read2_sequence]['seq_read_count'] += 1
+        read2_sequences = read_in_R2['seq_id']
+        for read2_sequence in read2_sequences:
+            sequences['name_list'][read2_sequence]['seq_read_count'] += 1
 
 
 def sequence_FPKM(sequences, amount_all_reads):
@@ -142,9 +123,25 @@ def sequence_FPKM(sequences, amount_all_reads):
         FPKM = sequence['seq_read_count'] / (convert_all_reads * convert_length)
         sequence['FPKM'] = FPKM
 
-    with open('bowtie2_sequqnces.json', 'w') as f:
-        json.dump(sequences, f)
-    f.close()
+    # with open(sys.argv[3], 'w') as f:
+    #     json.dump(sequences, f)
+    # f.close()
+    data_file = open(sys.argv[3], 'w')
+    csv_writer = csv.writer(data_file)
+    count = 0
+
+    seq_ids = sequences['name_list']
+    for seq_id in seq_ids.keys():
+        if count == 0:
+            # Writing headers of CSV file
+            header = ['seq_id']
+            header += seq_ids[seq_id].keys()
+            csv_writer.writerow(header)
+            count += 1
+        row = [seq_id] + list(seq_ids[seq_id].values())
+        csv_writer.writerow(row)
+    data_file.close()
+
 
 def cazyfamily_FPKM(cazyfamilies, amount_all_reads):
     for cazyfamily_name in cazyfamilies['name_list'].keys():
@@ -154,9 +151,28 @@ def cazyfamily_FPKM(cazyfamilies, amount_all_reads):
         FPKM = cazyfamily["cazyfamily_read_count"] / (convert_length * convert_all_reads)
         cazyfamily['FRKM'] = FPKM
 
-    with open('bowtie2_cazyfamilies.json', 'w') as f:
-        json.dump(cazyfamilies, f)
-    f.close()
+    # with open(sys.argv[4], 'w') as f:
+    #     json.dump(cazyfamilies, f)
+    # f.close()
+    data_file = open(sys.argv[4], 'w')
+    csv_writer = csv.writer(data_file)
+    count = 0
+
+    cazyfamily_names = cazyfamilies['name_list']
+    for cazyfamily_name in cazyfamily_names.keys():
+        if count == 0:
+            # Writing headers of CSV file
+            header = ['cazyfamily_name']
+            cazy_title =  list(cazyfamily_names[cazyfamily_name].keys())
+            del cazy_title[1]
+            header += cazy_title
+            csv_writer.writerow(header)
+            count += 1
+        cazy_info = list(cazyfamily_names[cazyfamily_name].values())
+        del cazy_info[1]
+        row = [cazyfamily_name] + cazy_info
+        csv_writer.writerow(row)
+    data_file.close()
 
 def main():
     cazyfamilies = {}
@@ -168,10 +184,10 @@ def main():
     sequences['seq_amount'] = 0
 
     start = time.time()
-    path_R1 = "output/diamond.new.R1.paf"
+    path_R1 = sys.argv[1]
     reads_R1 = get_brief_records(path_R1, cazyfamilies, sequences)
 
-    path_R2 = "output/diamond.new.R2.paf"
+    path_R2 = sys.argv[2]
     reads_R2 = get_brief_records(path_R2, cazyfamilies, sequences)
 
     seq_id_list_R1 = reads_R1.keys()
